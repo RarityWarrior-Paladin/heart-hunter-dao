3c2b29a0a200655519547a45c34239c06e7eae5f
Rabbit <alienworlds.cc1@gmail.com>
2022-06-30 17:31:00 +0800

    update wl

--- src/pages/home/useAllowlist.ts
+++ src/pages/home/useAllowlist.ts
@@ -3,14 +3,15 @@ import { Web3Context } from "../../context/web3-context"
 import { MerkleTree } from "merkletreejs";
 import { keccak256 } from "@ethersproject/keccak256";
 import allowlist from "../../assets/whitelist.json";
+const lowercaseAllowlist = allowlist.map((address) => address.toLowerCase())
+const leafNodes = allowlist.map((addr) => keccak256(addr));
+const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
+
 
 const useAllowlist = () => {
   const { account } = useContext(Web3Context)
   if (!account) return {isAllowlist: false, proof: []}
 
-  const lowercaseAllowlist = allowlist.map((address) => address.toLowerCase())
-  const leafNodes = allowlist.map((addr) => keccak256(addr));
-  const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
   const accountSafe = account.toLocaleLowerCase()
   const isAllowlist = lowercaseAllowlist.includes(accountSafe)
   const leaf = keccak256(accountSafe);
