b9005ad939e2ad5962afeaa684b2e46c597de549
RarityWarrior-Paladin <alienworlds.cc1@gmail.com>
2022-07-27 08:47:38 +0800

    x

--- src/solidity/auction.sol
+++ src/solidity/auction.sol
@@ -16,16 +16,15 @@ interface IERC721 {
     ) external;
 }
 
-struct BidInfo {
-    address account;
-    uint256 total;
-}
-
 contract LoveLovesAuction is Ownable, ERC721Holder {
 
-    mapping(address => uint256) public biders;
+    struct BidInfo {
+        address account;
+        uint256 total;
+    }
 
-    BidInfo[] public biderInfo;
+    mapping(address => uint256) internal _indexOfBider;
+    BidInfo[] internal _biders;
 
     bool public isOpen = false;
     uint256 public totalBurned = 0;
@@ -38,20 +37,36 @@ contract LoveLovesAuction is Ownable, ERC721Holder {
         loveLoves = IERC721(nftAddress);
     }
 
+    function indexByAddress(address account)
+        public
+        view
+        returns (uint256)
+    {
+        return _indexOfBider[account];
+    }
+
+    function biderInfo(address account)
+        public
+        view
+        returns (BidInfo memory)
+    {
+        uint256 index = indexByAddress(account);
+        BidInfo memory item = _biders[index];
+        return item;
+    }
+
     function bid(uint256[] calldata ids) public {
         require(isOpen, "not start");
         require(ids.length > 0, "need love");
 
-        if(
-            biderInfo[biders[msg.sender]].account != msg.sender
-        ){
-            biders[msg.sender] = biderInfo.length;
-            biderInfo.push(
+        if(biderInfo(msg.sender).account != msg.sender) {
+            _indexOfBider[msg.sender] = _biders.length;
+            _biders.push(
                 BidInfo(msg.sender, 0)
             );
         }
 
-        BidInfo storage info = biderInfo[biders[msg.sender]];
+        BidInfo memory info = biderInfo(msg.sender);
         info.total = info.total + ids.length;
         totalBurned = totalBurned + ids.length;
 
@@ -71,13 +86,13 @@ contract LoveLovesAuction is Ownable, ERC721Holder {
     }
 
     function totalBider() public view returns (uint256) {
-        return biderInfo.length;
+        return _biders.length;
     }
 
     function page(uint256 pageIndex, uint256 pageSize) public view returns (BidInfo[] memory list) {
         uint256 start = pageIndex * pageSize;
         for (uint i = 0; i<= pageSize ; i++) {
-            list[i] = biderInfo[start + i];
+            list[i] = _biders[start + i];
         }
         return list;
     }
